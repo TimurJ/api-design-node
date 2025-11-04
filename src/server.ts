51f28be5041fe678b05748f8514fe95702f4c07a
Timur Jalilov <timurjalilov1@gmail.com>
2025-11-04 13:02:12 +0000

    add routes for auth habits and user

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
 
 // Create Express application
 const app = express()
@@ -11,6 +14,10 @@ app.get('/health', (req, res) => {
     service: 'Habit Tracker API',
   })
 })
+
+app.use('/api/auth', authRoutes)
+app.use('/api/habit', habitRoutes)
+app.use('/api/user', userRoutes)
 
 // Export the app for use in other modules (like tests)
 export { app }
